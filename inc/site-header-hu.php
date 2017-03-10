0552da5f59aba2097c5dda65b5b9fe2cc184d1f6
hello <hello@fruccola.hu>
2017-03-10 05:13:24 -0500

    /nyitvatartas correction

--- inc/site-header-hu.php
+++ inc/site-header-hu.php
@@ -9,7 +9,7 @@
     <li class="menu"><a href="/etlap-itallap">Étlap-Itallap</a></li>
     <li class="map"><a href="/terkep">Térkép</a></li>
     <li class="contact"><a href="/kapcsolat">Kapcsolat</a><!-- <a class="button" href="tel://+3614306125">+36 1 430 6125</a> --></li>
-    <li class="open"><!-- class="warning" --><a href="nyitvatartas"><em>Nyitvatartás</em></a></li>
+    <li class="open"><!-- class="warning" --><a href="/nyitvatartas"><em>Nyitvatartás</em></a></li>
     <li class="happy"><a href="/jo-dolgok">Jó dolgok</a></li>
     <li class="sticky feedback"><a href="/visszajelzes" class="">Mit gondolsz rólunk?</a></li>
     <li class="sticky dailymenu"><a href="/napi-menu-ertesito" class="">Napi menü feliratkozás</a></li>
