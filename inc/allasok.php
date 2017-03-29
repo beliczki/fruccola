b0e0e28ae9ce5066970d9abb15ade4a89c79b7cb
hello <hello@fruccola.hu>
2017-03-28 23:45:21 -0400

    jobs, allasok

--- inc/allasok.php
+++ inc/allasok.php
@@ -1,74 +1,46 @@
 <section class="content jobs">
-  <h1>Állások</h1>
+  <h1>Legyél te is a fruccola csapat tagja</h1>
   <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Mit szólsz egy olyan munkahelyhez, amelyik szeret téged? Ahol egy jó csapat és finom ételek várnak. 
+    Ahol rugalmas beosztásban dolgozhatsz és két szabadnapod is megmarad. Ahol az estéid továbbra is a tieid lesznek. 
+    Ahol egy jó fizetés mellé további bónuszokat is kaphatsz. 
   </p>
   <ul class="items">
-      <li class="cook">
-        <h2>Szakács</h2>
-        <p>
-          <strong>AMIT MI KÍNÁLUNK:</strong><br />
-          kegyensúlyozott munkalégkör<br />
-          heti öt munkanap, két szabadnap<br />
-          magas bérezés<br />
-          havi rendszerű fizetés<br />
-          plusz jutalmazások<br />
-          korrekt munkáltatói hozzáállás<br />
-          hosszútávú munkalehetőség<br />
-          egészséges napi étkezés biztosítása<br />
-          <br />
-          <strong>FELADATOK, ELVÁRÁSOK:</strong><br />
-          pályakezdő vagy vendéglátásban szerzett szakácssegéd pozícióban betöltött korábbi tapasztalat<br />
-          nyersanyagok előkészítése, feldolgozása<br />
-          a’la cart ételek betanulása, elkészítése<br /><br />
-        </p>
-        <p><a href="mailto:hr@fruccola.hu?subject=Szakács, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
-      </li>
+    
       <li class="waiter">
-        <h2>Pincér</h2>
+        <h2>Pultos</h2>
         <p>
-          <strong>FAMIT MI KÍNÁLUNK:</strong><br />
-          kegyensúlyozott munkalégkör<br />
-          heti öt munkanap, két szabadnap<br />
-          havi rendszerű fizetés<br />
-          plusz jutalmazások<br />
-          korrekt munkáltatói hozzáállás<br />
-          hosszútávú munkalehetőség<br />
-          egészséges napi étkezés biztosítása<br />
-          <br />
-          <strong>FELADATOK, ELVÁRÁSOK:</strong><br />
-          vendéglátásban szerzett több éves tapasztalat<br />
-          csapatmunkára való szellem<br />
-          angol beszédszintű nyelvtudás<br />
-          rendelés felvétel, étel-ital elkészítés<br />
-          a vendégekkel való pozitív kapcsolattartás<br />
-          HACCP betartása<br />
-          kulturált megjelenés <br /><br />
+          Ha pultos szeretnél lenni nálunk, fontos, hogy jól beszélj angolul és bírd a pörgést. 
+          A tapasztalat hasznos, de a legfontosabb, hogy szeresd majd, amit csinálsz és kedves legyél a vendégeinkkel. 
+          Jó csapat vár majd. 
         </p>
         <p><a href="mailto:hr@fruccola.hu?subject=Pincér, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
       </li>
+    
+      <li class="cook">
+        <h2>Szakács</h2>
+        <p>
+          Ha jól mozogsz a különböző konyhák világában és neked is fontos a jó és friss alapanyag, 
+          vannak jó ötleteid, de szeretnéd, hogy az estéidet ne munkával töltsd, akkor gyere hozzánk. 
+        </p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Szakács, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+      </li>
+
       <li class="prep">
         <h2>Előkészítő</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Ha szeretsz konyhán dolgozni és lázba hoznak a friss gyümölcsök, zöldségek, akkor nálad a helyünk. 
         </p>
-        <ul class="required">
+        <!--<ul class="required">
           <li> Lorem ipsum dolor sit amet</li>
-        </ul>
+        </ul>-->
         <p><a href="mailto:hr@fruccola.hu?subject=Előkészítő, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
       </li>
       <li class="dishwasher">
         <h2>Mosogató</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Ha szereted a tisztaságot és fontos egy stabil munkahely, akkor csatlakozz hozzánk. 
         </p>
-        <ul class="required">
-          <li> Lorem ipsum dolor sit amet</li>
-        </ul>
         <p><a href="mailto:hr@fruccola.hu?subject=Mosogató, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
       </li>
     </ul>
--- inc/jobs.php
+++ inc/jobs.php
@@ -1,4 +1,53 @@
 <section class="content jobs">
+  <h1>Legyél te is a fruccola csapat tagja</h1>
+  <p>
+    Mit szólsz egy olyan munkahelyhez, amelyik szeret téged? Ahol egy jó csapat és finom ételek várnak. 
+    Ahol rugalmas beosztásban dolgozhatsz és két szabadnapod is megmarad. Ahol az estéid továbbra is a tieid lesznek. 
+    Ahol egy jó fizetés mellé további bónuszokat is kaphatsz. 
+  </p>
+  <ul class="items">
+    
+      <li class="waiter">
+        <h2>Cashier</h2>
+        <p>
+          Ha pultos szeretnél lenni nálunk, fontos, hogy jól beszélj angolul és bírd a pörgést. 
+          A tapasztalat hasznos, de a legfontosabb, hogy szeresd majd, amit csinálsz és kedves legyél a vendégeinkkel. 
+          Jó csapat vár majd. 
+        </p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Pincér, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+      </li>
+    
+      <li class="cook">
+        <h2>Cook</h2>
+        <p>
+          Ha jól mozogsz a különböző konyhák világában és neked is fontos a jó és friss alapanyag, 
+          vannak jó ötleteid, de szeretnéd, hogy az estéidet ne munkával töltsd, akkor gyere hozzánk. 
+        </p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Szakács, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+      </li>
+
+      <li class="prep">
+        <h2>Prep</h2>
+        <p>
+          Ha szeretsz konyhán dolgozni és lázba hoznak a friss gyümölcsök, zöldségek, akkor nálad a helyünk. 
+        </p>
+        <!--<ul class="required">
+          <li> Lorem ipsum dolor sit amet</li>
+        </ul>-->
+        <p><a href="mailto:hr@fruccola.hu?subject=Előkészítő, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+      </li>
+      <li class="dishwasher">
+        <h2>Dishwasher</h2>
+        <p>
+          Ha szereted a tisztaságot és fontos egy stabil munkahely, akkor csatlakozz hozzánk. 
+        </p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Mosogató, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+      </li>
+    </ul>
+</section>
+
+
+<section class="content jobs">
   <h1>Jobs</h1>
   <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
