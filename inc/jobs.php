fc3e77bb5ef0858090f2c8fec8623f99973d70a8
hello <hello@fruccola.hu>
2017-04-06 02:52:14 -0400

    jobs englsih part translation

--- inc/jobs.php
+++ inc/jobs.php
@@ -1,5 +1,5 @@
 <section class="content jobs">
-  <h1>Legyél te is a fruccola csapat tagja</h1>
+  <h1>Be part of the fruccola team</h1>
   <p>
     Mit szólsz egy olyan munkahelyhez, amelyik szeret téged? Ahol egy jó csapat és finom ételek várnak. 
     Ahol rugalmas beosztásban dolgozhatsz és két szabadnapod is megmarad. Ahol az estéid továbbra is a tieid lesznek. 
@@ -14,7 +14,7 @@
           A tapasztalat hasznos, de a legfontosabb, hogy szeresd majd, amit csinálsz és kedves legyél a vendégeinkkel. 
           Jó csapat vár majd. 
         </p>
-        <p><a href="mailto:hr@fruccola.hu?subject=Pultos, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Cashier, Jelentkezés, Fruccola, HR" class="button">Apply</a></p>
       </li>
     
       <li class="cook">
@@ -23,7 +23,7 @@
           Ha jól mozogsz a különböző konyhák világában és neked is fontos a jó és friss alapanyag, 
           vannak jó ötleteid, de szeretnéd, hogy az estéidet ne munkával töltsd, akkor gyere hozzánk. 
         </p>
-        <p><a href="mailto:hr@fruccola.hu?subject=Szakács, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Cook, Jelentkezés, Fruccola, HR" class="button">Apply</a></p>
       </li>
 
       <li class="prep">
@@ -34,70 +34,14 @@
         <!--<ul class="required">
           <li> Lorem ipsum dolor sit amet</li>
         </ul>-->
-        <p><a href="mailto:hr@fruccola.hu?subject=Előkészítő, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Prep, Jelentkezés, Fruccola, HR" class="button">Apply</a></p>
       </li>
       <li class="dishwasher">
         <h2>Dishwasher</h2>
         <p>
           Ha szereted a tisztaságot és fontos egy stabil munkahely, akkor csatlakozz hozzánk. 
         </p>
-        <p><a href="mailto:hr@fruccola.hu?subject=Mosogató, Jelentkezés, Fruccola, HR" class="button">Jelentkezés</a></p>
-      </li>
-    </ul>
-</section>
-
-
-<section class="content jobs">
-  <h1>Jobs</h1>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-  </p>
-  <ul class="items">
-      <li class="cook">
-        <h2>Cook</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        </p>
-        <ul class="required">
-          <li> Lorem ipsum dolor sit amet</li>
-        </ul>
-        <p><a href="mailto:hr@fruccola.hu?subject=Cook, apply, Fruccola, HR" class="button">Apply</a></p>
-      </li>
-      <li class="waiter">
-        <h2>Waiter</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        </p>
-        <ul class="required">
-          <li> Lorem ipsum dolor sit amet</li>
-        </ul>
-        <p><a href="mailto:hr@fruccola.hu?subject=Waiter, apply, Fruccola, HR" class="button">Apply</a></p>
-      </li>
-      <li class="prep">
-        <h2>Prep</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        </p>
-        <ul class="required">
-          <li> Lorem ipsum dolor sit amet</li>
-        </ul>
-        <p><a href="mailto:hr@fruccola.hu?subject=Prep, apply, Fruccola, HR" class="button">Apply</a></p>
-      </li>
-      <li class="dishwasher">
-        <h2>Dishwasher</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        </p>
-        <ul class="required">
-          <li> Lorem ipsum dolor sit amet</li>
-        </ul>
-        <p><a href="mailto:hr@fruccola.hu?subject=Dishwasher, apply, Fruccola, HR" class="button">Apply</a></p>
+        <p><a href="mailto:hr@fruccola.hu?subject=Dishwasher, Jelentkezés, Fruccola, HR" class="button">Apply</a></p>
       </li>
     </ul>
 </section>
