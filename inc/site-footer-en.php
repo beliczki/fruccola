2b163d84cf1d1a83a19d09ba59025fa80037f564
hello <hello@fruccola.hu>
2017-08-06 19:44:20 -0400

    site footer en hu

--- inc/site-footer-en.php
+++ inc/site-footer-en.php
@@ -9,34 +9,13 @@
 </nav>
 
 <nav class="locations">
-  <ul>
-      <li>
-          <a href="/map">Fruccola Arany János street</a>
-          <p>1051 Budapest, Arany János street 32.</p>
-          <a href="http://facebook.com/therealfruccola">facebook.com/<strong>therealfruccola</strong></a>
-          <ul class="social clearfix">
-              <li class="twitter"><a href="https://twitter.com/fruccola">Twitter</a></li>
-              <li class="facebook"> <a href="https://www.facebook.com/therealfruccola">Facebook</a></li>
-              <li class="instagram"><a href="http://instagram.com/hellofruccola">Instagram</a></li>
-              <li class="foursquare"><a href="http://4sq.com/9rdPXz">FourSquare</a></li>
-              <li class="googleplus"><a href="https://plus.google.com/118233606222629573878/">Google+</a></li>
-              <li class="tripadvisor"><a href="http://www.tripadvisor.com/Restaurant_Review-g274887-d7993522-Reviews-Fruccola-Budapest_Central_Hungary.html">TripAdvisor</a></li>
-              <li class="flickr"><a href="https://www.flickr.com/photos/fruccola/">Flickr</a></li>
-          </ul>
-      </li>
-      <li>
-          <a href="/map">Fruccola Kristóf square</a>
-          <p>1052 Budapest, Kristóf square 3.</p>
-          <a href="http://facebook.com/therealfruccola">facebook.com/<strong>therealfruccola</strong></a>
-          <ul class="social clearfix">
-              <li class="twitter"><a href="https://twitter.com/fruccola">Twitter</a></li>
-              <li class="facebook"><a href="https://www.facebook.com/therealfruccola">Facebook</a></li>
-              <li class="instagram"><a href="http://instagram.com/hellofruccola">Instagram</a></li>
-              <li class="foursquare"><a href="http://4sq.com/ohJzD3">FourSquare</a></li>
-              <li class="googleplus"><a href="https://plus.google.com/+FruccolaKristóftérBudapest/">Google+</a></li>
-              <li class="tripadvisor"><a href="http://www.tripadvisor.com/Restaurant_Review-g274887-d1124135-Reviews-Fruccola_Kristof_ter-Budapest_Central_Hungary.html">TripAdvisor</a></li>
-              <li class="flickr"><a href="https://www.flickr.com/photos/fruccola/">Flickr</a></li>
-          </ul>
-      </li>
+  <ul class="social clearfix">
+      <li class="twitter"><a href="https://twitter.com/fruccola">Twitter</a></li>
+      <li class="facebook"><a href="https://www.facebook.com/therealfruccola">Facebook</a></li>
+      <li class="instagram"><a href="http://instagram.com/hellofruccola">Instagram</a></li>
+      <li class="foursquare"><a href="https://foursquare.com/explore?mode=url&near=Budapest%2C%20Hungary&nearGeoId=72057594040982579&q=fruccola">FourSquare</a></li>
+      <li class="googleplus"><a href="https://plus.google.com/s/fruccola/">Google+</a></li>
+      <li class="tripadvisor"><a href="https://www.tripadvisor.com/Search?q=fruccola">TripAdvisor</a></li>
+      <li class="flickr"><a href="https://www.flickr.com/photos/fruccola/">Flickr</a></li>
   </ul>
 </nav>
