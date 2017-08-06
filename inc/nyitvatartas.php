35c208c40cc2efa7f049b96d661f8f6808578128
hello <hello@fruccola.hu>
2017-08-06 18:39:00 -0400

    nyitvatartas, open, mom

--- inc/nyitvatartas.php
+++ inc/nyitvatartas.php
@@ -3,7 +3,7 @@
   <ul>
     
     <li class="hours kristof clearfix">
-      <p><a href="fruccola-kristof-ter.html">Budapest, <strong>Kristóf tér 3.</strong></a></p>
+      <p><a href="https://goo.gl/maps/jecFsgn3was">Budapest, <strong>Kristóf tér 3.</strong></a></p>
       <h2>Kristóf tér</h2> 
       <h2 class="dayinfo"><span class="date">Ma:</span> <span class="tag"></span> <span class="time"></span></h2> 
 
@@ -31,7 +31,7 @@
     </li>
     
     <li class="hours arany clearfix">
-      <p><a href="fruccola-arany-janos-utca.html">Budapest, <strong>Arany János utca 32.</strong></a></p>
+      <p><a href="https://goo.gl/maps/8H4t76ZAyJB2">Budapest, <strong>Arany János utca 32.</strong></a></p>
       <h2>Arany János utca</h2> 
       <h2 class="dayinfo"><span class="date">Ma:</span> <span class="tag"></span> <span class="time"></span></h2> 
 
@@ -57,6 +57,35 @@
         </table>
       </div>
     </li>
+    
+    <li class="hours mom clearfix">
+      <p><a href="https://goo.gl/maps/CqNa8dKYjkx">Budapest, <strong>MOM park, Liszabon sétány</strong></a></p>
+      <h2>MOM park</h2> 
+      <h2 class="dayinfo"><span class="date">Ma:</span> <span class="tag"></span> <span class="time"></span></h2> 
+
+      <div class="calendar">
+          <h3></h3>
+          <div class="weekHeader clearfix">
+              <div class="day">H:</div>
+              <div class="day">K:</div>
+              <div class="day">Sze:</div>
+              <div class="day">Cs:</div>
+              <div class="day">P:</div>
+              <div class="day">Szo:</div>
+              <div class="day">V:</div>
+          </div>
+          <div class="week clearfix">
+          </div>
+          <a class="button changeMonth next"></a>
+      </div>
+      <div class="table">
+        <h3>Normál nyitvatartási rend</h3>
+        <table>
+          
+        </table>
+      </div>
+    </li>
+    
   </ul>
 
 </article>
--- inc/open.php
+++ inc/open.php
@@ -3,7 +3,7 @@
   <ul>
     
     <li class="hours kristof clearfix">
-      <p><a href="fruccola-kristof-square.html">Budapest, <strong>Kristóf square 3.</strong></a></p>
+      <p><a href="https://goo.gl/maps/jecFsgn3was">Budapest, <strong>Kristóf square 3.</strong></a></p>
       <h2>Kristóf square</h2> 
       <h2 class="dayinfo"><span class="date">Today:</span> <span class="tag"></span> <span class="time"></span></h2> 
 
@@ -31,7 +31,7 @@
     </li>
     
     <li class="hours arany clearfix">
-      <p><a href="fruccola-arany-janos-street.html">Budapest, <strong>Arany János street 32.</strong></a></p>
+      <p><a href="https://goo.gl/maps/8H4t76ZAyJB2">Budapest, <strong>Arany János street 32.</strong></a></p>
       <h2>Arany János street</h2> 
       <h2 class="dayinfo"><span class="date">Today:</span> <span class="tag"></span> <span class="time"></span></h2> 
 
@@ -57,6 +57,35 @@
         </table>
       </div>
     </li>
+    
+    <li class="hours mom clearfix">
+      <p><a href="https://goo.gl/maps/CqNa8dKYjkx">Budapest, <strong>MOM park, Liszabon promenade</strong></a></p>
+      <h2>MOM park</h2> 
+      <h2 class="dayinfo"><span class="date">Today:</span> <span class="tag"></span> <span class="time"></span></h2> 
+
+      <div class="calendar">
+          <h3></h3>
+          <div class="weekHeader clearfix">
+              <div class="day">Mon</div>
+              <div class="day">Tue</div>
+              <div class="day">Wed</div>
+              <div class="day">Thu</div>
+              <div class="day">Fri</div>
+              <div class="day">Sat</div>
+              <div class="day">Sun</div>
+          </div>
+          <div class="week clearfix">
+          </div>
+          <a class="button changeMonth next"></a>
+      </div>
+      <div class="table">
+        <h3>Normal Opening hours</h3>
+        <table>
+         
+        </table>
+      </div>
+    </li>
+    
   </ul>
 
 </article>
