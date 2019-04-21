841b5b3fc2e1228c0913cd358e9fdfbc8c67a91f
Bely <beliczki.robert@gmail.com>
2019-04-21 15:40:59 +0200

    allergen info menu oldal aljára, magyar angol

--- inc/menu.php
+++ inc/menu.php
@@ -43,10 +43,12 @@
 <section class="content menu allergenes">
     <h2>
       Allergens:
-      <em class="flourfree" title="Flour free">Flour free</em>
-      <em class="lactosefree" title="Lactose free">Lactose free</em>
       <em class="vegetarian" title="Vegetarian">Vegetarian</em>
       <em class="vegan" title="Vegan">Vegan</em>
+      <em class="flourfree" title="Flour free">Flour free</em>
+      <em class="lactosefree" title="Lactose free">Lactose free</em>
+      <em class="sugarfree" title="Sugar free">Sugar free</em>
+      <em class="hot" title="Spicy">Spicy</em>
     </h2>
     <p>
         All our food is marked to show if it’s free of flour, lactose, or sugar, and if it’s vegetarian or vegan. That said, please bear in mind that some of our raw materials are prepared in an environment that processes wheat and other wheat-containing cereals, nuts, peanuts, milk, eggs, soy, sesame seeds, mustard, celery, lupin, fish, crustaceans, or molluscs, and may therefore contain traces of the above.
@@ -84,10 +86,12 @@
 <section class="content menu allergenes">
     <h2>
       Allergének:
-      <em class="flourfree" title="Lisztmentes">Lisztmentes</em>
-      <em class="lactosefree" title="Laktózmentes">Laktózmentes</em>
       <em class="vegetarian" title="Vegetáriánus">Vegetáriánus</em>
       <em class="vegan" title="Vegán">Vegán</em>
+      <em class="flourfree" title="Lisztmentes">Lisztmentes</em>
+      <em class="lactosefree" title="Laktózmentes">Laktózmentes</em>
+      <em class="sugarfree" title="Sugar free">Cukormentes</em>
+      <em class="hot" title="Fűszeres, Csípős">Fűszeres, Csípős</em>
     </h2>
     <p>
         Minden ételünkön feltüntetjük ha liszt-, laktóz- vagy cukormentes, vegetáriánus vagy vegán. Ennek ellenére kérjük, vedd figyelembe, hogy alapanyagaink egy része búza-, és gluténtartalmú gabonaféléket, dióféléket, földimogyorót, tejet, tojást, szóját, szezámmagot, mustárt, zellert, csillagfürtöt, halat, rákféléket, puhatestűeket is feldolgozó üzemben készült, így ezeket nyomokban tartalmazhatja.
